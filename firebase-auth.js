69907f7815fd47bba915fb1d55a29a62b37cd14c
rangelrojas <rangelrojas@unicesar.edu.co>
2024-04-08 17:56:57 -0500

    Autenticacion por correo

---
+++ firebase-auth.js
@@ -0,0 +1,46 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDp1agRZmYWvPBXaINLILHtwlpZKgAJMVw",
+  authDomain: "fb-notas.firebaseapp.com",
+  projectId: "fb-notas",
+  storageBucket: "fb-notas.appspot.com",
+  messagingSenderId: "36933207721",
+  appId: "1:36933207721:web:58705b3a46ee4949444bb8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const signUpButton = document.querySelector('#signUpButton');
+
+    signUpButton.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        // Obtener el valor del email y la contraseña
+        const email = document.querySelector('#emailInput').value;
+        const password = document.querySelector('#passwordInput').value;
+
+        // Crear usuario con correo y contraseña
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Usuario creado exitosamente
+                var user = userCredential.user;
+                console.log('Usuario creado:', user);
+
+                // Aquí puedes redirigir al usuario o mostrar un mensaje de éxito
+            })
+            .catch((error) => {
+                // Error al crear el usuario
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.error('Error al crear el usuario:', errorMessage);
+                // Aquí puedes manejar los errores de autenticación
+            });
+    });
+});
